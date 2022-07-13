ac9ab74d480b53ed9d95189e78ed66518bf7dfaf
arjun.kdev <arjun.kdev@quest-global.com>
2022-07-13 13:05:33 +0530

    release db

--- header/customer_file_operations.h
+++ header/customer_file_operations.h
@@ -4,3 +4,7 @@
 void customer_bdb_readall(customer *customerAddr, int *customerCount);
 void add_customer_intoFile(customer *customerAddr);
 void update_customerinfo_intoFile(customer *customerAddr,char *username);
+void update_customer_bdb_update_intoFile(customer *customerAddr,char *username);
+
+
+
--- header/loan_application_bdb_file_operation.h
+++ header/loan_application_bdb_file_operation.h
@@ -2,6 +2,7 @@
 #include"finance_t.h"
 
 void loan_application_bdb_insert(application *applicationAddr);
+void release_loan_application_bdb_insert(releaseLoan *releaseloanAddr);
 void loan_application_bdb_readAll(application *loanApplicationList,int *noOfObjects);
 int loan_application_bdb_readByName(application* loanApplicationAddr, char applicationCustNameAddr[]);
 int loan_application_bdb_readByEmail(application* loanApplicationAddr, char applicationCustEmailAddr[]);
